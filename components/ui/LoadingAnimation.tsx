bf8ee0b421ca5cd4481143ad257eaa95d908823d
81782641082461074508273912378186470 <admin@autolaku.com>
2025-01-10 21:23:30 +0700

    21361892370981273123123

--- components/ui/LoadingAnimation.tsx
+++ components/ui/LoadingAnimation.tsx
@@ -6,7 +6,7 @@ const LoadingAnimation = ({ text }: { text?: string }) => {
   return (
     <div className="fixed top-0 left-0 z-[100] w-screen h-screen flex flex-col justify-center items-center bg-dark-800">
       <div className="relative flex flex-col items-center justify-center gap-3">
-        <div className="w-20 h-20 border-y-[1px] border-y-white/30 border-x-2 border-x-white rounded-full animate-spin"></div>
+        {/* <div className="w-20 h-20 border-y-[1px] border-y-white/30 border-x-2 border-x-white rounded-full animate-spin"></div> */}
         <Image
           src={ALogo}
           alt="Loading Logo | Dokmai Store"
