241b65bd317363bbad735bab48d9b4245a741d1f
81782641082461074508273912378186470 <admin@autolaku.com>
2025-02-22 09:08:37 +0700

    52342342

--- components/auth/Daftar.tsx
+++ components/auth/Daftar.tsx
@@ -225,11 +225,17 @@ const Daftar = () => {
                   </button>
                 </div>
               </div>
-              <button
+              {/* <button
                 type="button"
                 onClick={handleContinue}
                 className="bg-white text-sm lg:text-base text-black font-bold py-2 px-4 rounded w-full">
                 Lanjutkan Pendaftaran
+              </button> */}
+              <button
+                type="button"
+                disabled
+                className="bg-white text-sm lg:text-base text-black font-bold py-2 px-4 rounded w-full cursor-not-allowed opacity-65">
+                Untuk Sementara Pendaftaran Ditutup
               </button>
             </>
           )}
