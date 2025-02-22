2b9055f2de207e04510c68ef0a4c7927ef7839b4
81782641082461074508273912378186470 <admin@autolaku.com>
2025-02-22 20:38:18 +0700

    23423423423

--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -20,17 +20,17 @@ const HeroSection = () => {
       />
       <div className="max-w-screen-xl mx-auto flex flex-col items-center justify-start h-full text-center text-white z-10 px-5 gap-16 mt-24 lg:mt-32">
         <div className="flex flex-col items-center justify-center">
-          <h1 className="lg:text-6xl md:text-5xl text-xl font-extrabold mb-4 __gradient_text">
+          <h1 className="lg:text-6xl md:text-5xl text-2xl font-extrabold mb-2 __gradient_text">
             Dropship ribet bikin kalah saing?
           </h1>
-          <p className="lg:text-3xl md:text-xl flex text-light-800">
+          <h3 className="lg:text-3xl md:text-xl flex text-light-800">
             Sekarang bisa
             <strong className="flex flex-col items-center justify-center mx-2 text-white">
               cepat & efisien
               <div className="brush-underline" />
             </strong>
             tanpa alat lain.
-          </p>
+          </h3>
         </div>
         <div className="relative w-full max-w-3xl h-[500px] -mt-36 md:-mt-24 lg:-mt-20">
           <Image
@@ -42,27 +42,41 @@ const HeroSection = () => {
             draggable={false}
           />
         </div>
-        <div className="flex w-fit justify-center items-center gap-5 -mt-44 md:-mt-32 lg:-mt-20">
-          <DownloadButton
-            className="!bg-white rounded-full"
-            downloadUrl="/api/download?platform=macos"
-            buttonText={
-              <div className="flex gap-2 text-black text-xs lg:text-xl lg:gap-3 justify-center items-center w-fit">
-                <DiApple className="text-lg" /> MacOS
-              </div>
-            }
-            downloadingText="Mengunduh..."
-          />
-          <DownloadButton
-            className="!bg-blue-500 rounded-full"
-            downloadUrl="/api/download?platform=windows"
-            buttonText={
-              <div className="flex gap-2 text-white text-xs lg:text-xl lg:gap-3 justify-center items-center w-fit">
-                <DiWindows className="text-lg" /> Windows
-              </div>
-            }
-            downloadingText="Mengunduh..."
-          />
+        <div className="w-full flex-col justify-center items-center gap-5 -mt-44 md:-mt-32 lg:-mt-20">
+          <h2 className="text-xs whitespace-normal lg:px-80 text-white/70">
+            Autolaku adalah platform dropshipping canggih yang dirancang khusus untuk mempermudah
+            dan mengoptimalkan proses dropshipping di Indonesia. Platform ini mengintegrasikan
+            teknologi kecerdasan buatan (AI) dari xAI dan OpenAI untuk otomatisasi cerdas, seperti
+            pembuatan deskripsi produk yang menarik, analisis pasar, dan dukungan pelanggan
+            responsif, yang memungkinkan pengguna menghemat waktu dan meningkatkan efisiensi. Selain
+            itu, Autolaku memanfaatkan layanan Cloudinary untuk pengolahan gambar, memastikan gambar
+            produk dioptimalkan dengan cepat, aman, dan siap digunakan di berbagai marketplace tanpa
+            hambatan teknis. Dengan aplikasi desktop yang tersedia untuk macOS dan Windows, Autolaku
+            menawarkan solusi terpadu yang menghilangkan kerumitan alat terpisah, memungkinkan
+            dropshipper fokus pada pertumbuhan bisnis mereka.
+          </h2>
+          <div className="flex mt-5 justify-center items-center w-full">
+            <DownloadButton
+              className="!bg-white rounded-full"
+              downloadUrl="/api/download?platform=macos"
+              buttonText={
+                <div className="flex gap-2 text-black text-xs lg:text-xl lg:gap-3 justify-center items-center w-fit">
+                  <DiApple className="text-lg" /> MacOS
+                </div>
+              }
+              downloadingText="Mengunduh..."
+            />
+            <DownloadButton
+              className="!bg-blue-500 rounded-full"
+              downloadUrl="/api/download?platform=windows"
+              buttonText={
+                <div className="flex gap-2 text-white text-xs lg:text-xl lg:gap-3 justify-center items-center w-fit">
+                  <DiWindows className="text-lg" /> Windows
+                </div>
+              }
+              downloadingText="Mengunduh..."
+            />
+          </div>
         </div>
       </div>
     </section>
