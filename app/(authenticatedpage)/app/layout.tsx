e447c9f0ca19bc612705da86a2402983b64140ae
81782641082461074508273912378186470 <admin@autolaku.com>
2025-04-27 04:39:49 +0700

    532423

--- app/(authenticatedpage)/app/layout.tsx
+++ app/(authenticatedpage)/app/layout.tsx
@@ -3,6 +3,7 @@ import { AuthDashboardProvider } from '@/context/AuthDashboardContext';
 
 import '@/styles/globals.css';
 import '@/styles/fonts.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,6 +12,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="min-h-screen bg-black text-white selection:bg-white/65 selection:text-black no-scrollbar">
+        <Analytics />
         <div className="flex flex-col justify-center items-center w-full pt-12">
           <AuthDashboardProvider>
             <DashboardWrapper>{children}</DashboardWrapper>
