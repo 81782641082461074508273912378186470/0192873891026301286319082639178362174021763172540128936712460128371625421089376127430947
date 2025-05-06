fec3f35508d8e99028a578ec85ea7fd970c26d52
81782641082461074508273912378186470 <admin@autolaku.com>
2025-05-06 22:09:04 +0700

    63453467345

--- lib/UsersUtils.ts
+++ lib/UsersUtils.ts
@@ -1,5 +1,5 @@
 export async function showLicenses(token: string) {
-  const URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+  const URL = process.env.NEXT_PUBLIC_API_URL || 'https://autolaku.com';
   try {
     const response = await fetch(`${URL}/api/users/licenses`, {
       method: 'GET',
@@ -8,15 +8,15 @@ export async function showLicenses(token: string) {
         Authorization: `Bearer ${token}`,
       },
     });
-
+    console.log(`${URL}/api/users/licenses`);
     if (!response.ok) {
       const errorData = await response.json();
-      //   console.log('showLicenses:', errorData);
+      console.log('showLicenses:', errorData);
       throw new Error(errorData.error || 'Failed to retrieve licenses');
     }
 
     const data = await response.json();
-    // console.log('showLicenses:', data);
+    console.log('showLicenses:', data);
     return data.licenses;
   } catch (error) {
     console.error('Error retrieving licenses:', error);
